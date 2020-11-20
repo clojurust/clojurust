17f91bc2037dcc8b21fb4720c97c773cecfd47b8
ivanpierre <me@ivanpierre.world>
2020-11-20 03:37:49 +0100

    Simplify Keyword

--- src/clojure/rust/keywords.rs
+++ src/clojure/rust/keywords.rs
@@ -1,62 +1,41 @@
 //! clojure::rust::keyword: keyword index
 
 use std::cell::RefCell;
-
 use im::vector::Vector;
 use im::hashmap::HashMap;
+use core::cell::RefMut;
 
-#[derive(Debug)]
-struct KeywordsVect {
-    data: Vector<&'static str>,
+struct Keywords {
+    map: HashMap<&'static str, usize>,
+    vect: Vector<&'static str>,
 }
-
-impl<'i> KeywordsVect {
-    fn new() -> KeywordsVect {
-        KeywordsVect {
-            data: Vector::<&'static str>::new(),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct KeywordsMap {
-    data: HashMap<&'static str, usize>
-}
-
-impl<'i> KeywordsMap {
-    fn new() -> KeywordsMap {
-        KeywordsMap {
-            data: HashMap::<&'static str, usize>::new(),
-        }
-    }
-}
-
-struct Keywords {}
 
 impl Keywords {
+    fn new() -> Keywords {
+        Keywords {
+            map: HashMap::<&'static str, usize>::new(),
+            vect: Vector::<&'static str>::new(),
+        }
+    }
+
     pub fn len() -> usize {
-        keywords_vect.with(|vect| 
-            {
-            vect.borrow().data.len()
+        keywords.with(|kw| {
+            kw.borrow().vect.len()
         })
     }
     
     pub fn get(key: &'static str) -> usize {
-        keywords_map.with(|map| {
-            let mut m = map.borrow_mut();
+        let length = Keywords::len();
+        keywords.with(|kw| {
+            let mut m = kw.borrow_mut();
             
-            match m.data.get(key) {
+            match m.map.get(key) {
                 // found entry
                 Some(idx) => { *idx }
                 None => {
                     // add entry in vect and map
-                    let length = Keywords::len();
-                    
-                    keywords_vect.with(|vect| {
-                        vect.borrow_mut().data.insert(length, key);
-                    });
-                    
-                    m.data.insert(key, length);
+                    m.vect.insert(length, key);
+                    m.map.insert(key, length);
                     
                     // return new index
                     length
@@ -66,8 +45,8 @@ impl Keywords {
     }
 
     pub fn to_string(index: usize) -> &'static str {
-        keywords_vect.with(|vect| {
-            match vect.borrow().data.get(index) {
+        keywords.with(|kw| {
+            match kw.borrow().vect.get(index) {
                 Some(key) => { *key }
                 None => {""}
             }
@@ -82,14 +61,8 @@ impl Keywords {
 // Test with thread-local for now, change to Atom afterward
 thread_local!(
     #[allow(non_upper_case_globals)]
-    static keywords_vect: RefCell<KeywordsVect> = 
-                        RefCell::new(KeywordsVect::new()));
-
-thread_local!(
-    #[allow(non_upper_case_globals)]
-    static keywords_map: RefCell<KeywordsMap> = 
-                        RefCell::new(KeywordsMap::new())
-                    );
+    static keywords: RefCell<Keywords> = 
+                        RefCell::new(Keywords::new()));
 
 #[test]
 fn test_the_thing() {
@@ -97,6 +70,11 @@ fn test_the_thing() {
     Keywords::init_static(); // expected to succeed
     println!("New state {:?}", Keywords::len());
     Keywords::get("essai");
-    println!("add essai {:?} {}", Keywords::len(), Keywords::to_string(0));
+    println!("add essai {:?} {}", Keywords::len(), 
+                                    Keywords::to_string(0));
+    Keywords::get("essai2");
+    println!("add essai2 {:?} {} {}", Keywords::len(), 
+                                    Keywords::to_string(0),
+                                    Keywords::to_string(1));
     assert_eq!(1,1)
 }
